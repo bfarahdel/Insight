2ce789e95bd82372b68a30d7ad90b634eb175358
bfarahdel <britny205@gmail.com>
2022-01-02 01:18:40 -0500

    Separated the 3D models into individual figures with captions.

--- app.py
+++ app.py
@@ -43,13 +43,17 @@ def echo_models():
 
     return flask.render_template(
         "ar_models.html",
+        ent_len=3,
         echo_medical=echo_medical,
+        med_entry=["d3e5b67c-061f-4977-9b3a-8d4027f80905", "6f276bf7-d203-4676-8813-df72f680ae8c", "1861a637-1e6e-43af-b7fd-2e87a67bb225"],
+        med_name=["Heart", "Lungs (Exhale)", "Human Immunodeficiency Virus (HIV)"],
         echo_geo=echo_geo,
+        geo_entry=["2c34c3ae-d390-4b7a-af82-706eb2e6103d", "4e2552af-6c7d-4670-bbbc-617b823b855b", "247a7845-f12b-4a75-9879-370af28f4aa1"],
+        geo_name=["US Capitol","Buckingham Palace", "Great Wall of China"],
     )
 
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"),
         port=os.getenv("PORT", "8088"),
-        debug=True,
     )
